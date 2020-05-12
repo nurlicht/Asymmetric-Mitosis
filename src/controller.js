558df65cf9e93b9663c9f3eeb8b94e3671d644a3
Aliakbar Jafarpour <jafarpour.a.j@ieee.org>
2020-05-12 10:08:07 +0200

    Moving the Animator to the controller module

--- src/controller.js
+++ src/controller.js
@@ -139,3 +139,49 @@ class TrackMTRenderer {
 		return ctx;
 	}
 }
+
+class Animator {
+    constructor(geometry, view) {
+        this.view = view;
+        this.reset(geometry);
+    }
+
+    reset(geometry) {
+        if(this.timerID) clearInterval(this.timerID);
+        this.view.reset();
+        this.spindle = this.getSpindle(geometry);
+        this.showValues(geometry);
+        this.view.ctx = SpindleRenderer.render(this.view.ctx, this.spindle);
+        this.timerID = this.getTimerID(this.view.ctx, this.view.plot, geometry);
+    }
+
+    getTimerID(ctx, plot, geometry) {
+        const callback = this.drawAll.bind(this, this);
+        return setInterval(callback, 1000 / geometry.fps);
+    }
+
+    getSpindle(geometry) {
+        return new Spindle(geometry.N, geometry.nEBs, geometry);
+    }
+
+    showValues(geometry) {
+        let utilities = new Utilities();
+        utilities.d$V("nMT", geometry.N);
+        utilities.d$V("FractionEBs", utilities.limitDP(geometry.Asymmetry[2]));
+    }
+
+    drawAll(instance) {
+        instance.view.initialize();
+        instance.spindle.update();
+        instance.view.ctx = SpindleRenderer.render(instance.view.ctx, instance.spindle);
+        instance.view.plot.draw(instance.spindle.orientations, instance.spindle.nOrientations);
+    }
+
+    static create() {
+        const geometry = new Geometry();
+        const animator = new Animator(geometry, new View());
+        const update = ()=> animator.reset(geometry);
+        animator.controller = new Controller(geometry, update);
+        return animator;
+    }
+}
